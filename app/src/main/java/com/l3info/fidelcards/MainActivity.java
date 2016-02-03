d860efcf549e98eaff894f363bbe7a696dc53aa4
Draxxo <killer76520@gmail.com>
2016-02-03 10:09:54 +0100

    commentaire enlevé

--- app/src/main/java/com/l3info/fidelcards/MainActivity.java
+++ app/src/main/java/com/l3info/fidelcards/MainActivity.java
@@ -2,6 +2,8 @@ package com.l3info.fidelcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
